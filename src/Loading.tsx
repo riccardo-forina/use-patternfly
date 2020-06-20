0c470cc873936c970b17280c717d2cce9f33ebf1
ajaypratap003 <ajay.pratap233@gmail.com>
2020-06-20 23:29:00 +0530

    change loading message

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -12,7 +12,7 @@ export const Loading: React.FunctionComponent = () => (
   <PageSection aria-label="Loading Content Container">
     <EmptyState variant={EmptyStateVariant.full}>
       <EmptyStateIcon variant="container" component={Spinner} />
-      <Title size="lg" headingLevel="h1">········Loading········</Title>
+      <Title size="lg" headingLevel="h1">Loading...</Title>
     </EmptyState>
   </PageSection>
 );
